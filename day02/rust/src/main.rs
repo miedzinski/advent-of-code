d8fc6e488e14feaa0b6188d6171776171f6f001d
Dominik Miedziński <dominik@mdzn.pl>
2017-12-02 11:23:20 +0100

    day 2 part 2

--- day02/rust/src/main.rs
+++ day02/rust/src/main.rs
@@ -27,6 +27,33 @@ fn part1<Grid: AsRef<[Row]>, Row: AsRef<[usize]>>(rows: Grid) -> usize {
         .sum()
 }
 
+fn sorted_pairs<T: Copy + Ord>(nums: &[T]) -> Vec<(T, T)> {
+    let mut v = vec![];
+    for (idx, x) in nums.iter().enumerate() {
+        for y in &nums[idx + 1..] {
+            if x > y {
+                v.push((*x, *y))
+            } else {
+                v.push((*y, *x))
+            }
+        }
+    }
+    v
+}
+
+fn part2<Grid: AsRef<[Row]>, Row: AsRef<[usize]>>(rows: Grid) -> usize {
+    rows.as_ref()
+        .iter()
+        .map(|row| {
+            sorted_pairs(row.as_ref())
+                .iter()
+                .find(|&&(a, b)| a % b == 0)
+                .map(|&(a, b)| a / b)
+                .unwrap()
+        })
+        .sum()
+}
+
 fn main() {
     let rows: Vec<Vec<_>> = INPUT
         .lines()
@@ -37,4 +64,5 @@ fn main() {
         })
         .collect();
     println!("part1: {}", part1(&rows));
+    println!("part2: {}", part2(&rows));
 }
